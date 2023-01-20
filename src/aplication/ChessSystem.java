0d9e81d6b238deed0d20c808167327863ea4c518
Caio Vinicius <caioviniciussousadema@gmail.com>
2023-01-19 21:53:59 -0300

    First class: Position

--- src/aplication/ChessSystem.java
+++ src/aplication/ChessSystem.java
@@ -1,9 +1,13 @@
 package aplication;
 
+import boardgame.Position;
+
 public class ChessSystem {
 
     public static void main(String[] args) {
-        System.out.println("hellow word!");
+        Position position = new Position(2, 1);
+        
+        System.out.println(position);
     }
     
 }
